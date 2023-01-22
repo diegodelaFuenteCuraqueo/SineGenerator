f441ee71be0a9dc76c359d328c7574689d3e3393
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2023-01-22 20:27:03 -0300

    Initial Commit

---
+++ SineGenerator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SineGenerator
+//
+//  Created by Diego Ignacio de la Fuente Curaqueo on 22-01-23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
